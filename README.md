25525a90d5d62a4f1ba8965d7e2d881f5e15a600
petry-piera <petry.m.sianipar@gdpventure.com>
2013-02-06 13:07:44 +0700

    add script mysql.sh and make some instruction in README

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ installation
 ============
 
 This repository help you to install Nginx and MySQL
+
+1. Add executables flag to the mysql.sh script (i.e., chmod 755)
+2. Run the mysql.sh script
---
+++ mysql.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# install MySQL client on Amazon Linux AMI
+# Created by Petry Sianipar
+ 
+function init(){
+	which mysql
+	if [ $? == 0 ]
+	then
+	echo "MySQL has been installed on your machine"
+	exit 0
+	fi
+}
+
+function install(){
+	sudo rpm -ivh $1
+}
+ 
+function finish(){
+	cd /home/ec2-user
+	echo "Success Installing MySQL"
+	rpm -qa | grep MySQL
+	rm *.rpm
+}
+ 
+init
+install MySQL-client-5.1.63-1.glibc23.x86_64.rpm
+install MySQL-devel-5.1.63-1.glibc23.x86_64.rpm
+install MySQL-embedded-5.1.63-1.glibc23.x86_64.rpm
+install MySQL-shared-5.1.63-1.glibc23.x86_64.rpm
+install MySQL-test-5.1.63-1.glibc23.x86_64.rpm
+finish
